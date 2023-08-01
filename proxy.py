5a886eb79e452d9a4fcf564b54774329f36eb28a
Dinhkhoi2603 <139414597+Dinhkhoi2603@users.noreply.github.com>
2023-08-01 10:26:17 +0700

    Add files via upload

---
+++ proxy.py
@@ -0,0 +1,83 @@
+import socket
+import threading
+import ssl
+
+def handle_client(client_socket):
+    request_data = client_socket.recv(4096)
+    request_lines = request_data.split(b'\r\n')
+
+    
+    
+    # Tìm kiếm URL và phương thức yêu cầu
+    request_line = request_lines[0].decode()
+    print(request_line)
+    
+    request_method, url, _ = request_line.split()
+    #if url==data:
+        #return  
+    # Xử lý URL
+    # http://oosc.online/
+
+    if url.startswith("http://"):
+        url = url[7:]
+    elif url.startswith("https://"):
+        url = url[8:]
+
+    # Tách thông tin hostname và port (nếu có)
+    #testphp.vulnweb.com/login.php
+    if '/' in url:
+        host_path = url.split('/', 1)
+        host = host_path[0]
+        path = '/' + host_path[1]
+        #oosc.online
+    else:
+        host = url
+        path = '/'
+    #kshdfskjh:12124
+    if ':' in host:
+        remote_host, remote_port = host.split(':', 1)
+        remote_port = int(remote_port)
+    else:
+        remote_host = host
+        remote_port = 80
+
+    # Kết nối tới máy chủ web từ xa
+    remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    remote_socket.connect((remote_host, remote_port))
+
+    # Gửi yêu cầu HTTP tới máy chủ web từ xa
+    # GET / HTTP/1.1\r\nHost: oosc.online \r\n\r\n 
+    remote_request = f"{request_method} {path} HTTP/1.1\r\nHost: {host}\r\n\r\n".encode()
+    remote_socket.sendall(remote_request)
+
+    # Nhận phản hồi từ máy chủ web từ xa và gửi lại cho trình duyệt
+    while True:
+        remote_data = remote_socket.recv(4096)
+        if len(remote_data) == 0:
+            break 
+        client_socket.send(remote_data)
+
+    # Đóng kết nối
+    remote_socket.close()
+    client_socket.close()
+
+def proxy_server():
+    local_host = '127.0.0.1'  # Địa chỉ IP của máy cục bộ (localhost)
+    local_port = 8000  # Cổng để lắng nghe các kết nối từ trình duyệt
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Tạo socket TCP
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Cho phép sử dụng lại địa chỉ cổng ngay khi socket bị đóng
+    server_socket.bind((local_host, local_port))  # Ràng buộc socket tới địa chỉ IP và cổng cục bộ
+    server_socket.listen(5)  # Lắng nghe kết nối từ trình duyệt, giới hạn đợi đến 5 kết nối
+
+    print(f"Proxy đang lắng nghe trên {local_host}:{local_port}")
+
+    while True:
+        client_socket, client_addr = server_socket.accept()  # Chấp nhận kết nối mới từ trình duyệt
+        print(f"Kết nối mới từ {client_addr[0]}:{client_addr[1]}")
+
+        client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+        client_handler.start()  # Bắt đầu xử lý kết nối từ trình duyệt
+
+if __name__ == "__main__":
+    proxy_server()
