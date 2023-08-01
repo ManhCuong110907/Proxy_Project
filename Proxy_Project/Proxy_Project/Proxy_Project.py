fe77bed641ccad507cabdf4a85330915c4eb2396
ManhCuong110907 <manhcuong110907@gmail.com>
2023-08-01 17:03:02 +0700

    update main

--- Proxy_Project/Proxy_Project/Proxy_Project.py
+++ Proxy_Project/Proxy_Project/Proxy_Project.py
@@ -1,7 +1,9 @@
 ﻿import socket
 import threading
 import ssl
+import os
 from datetime import datetime, time
+cache_data = {}
 config = {}
 # Hàm đọc file config
 def read_config_file(file_path, config):
@@ -27,6 +29,7 @@ def is_within_time_range(config):
     current_time = datetime.now()
     return config['start_time'] <= current_time.hour <= config['end_time']
 
+# Hàm truy cập 403
 def Error403(client_socket):
     with open('error403.html', 'r') as file:
             response_data = file.read()
@@ -35,45 +38,42 @@ def Error403(client_socket):
     response = f"HTTP/1.1 200 OK\r\nContent-Length: {len(response_data)}\r\n\r\n{response_data}"
     client_socket.sendall(response.encode())
 
+# Hàm xử lý input
 def handle_client(client_socket):
     request_data = client_socket.recv(4096)
     request_lines = request_data.split(b'\r\n')
 
     # Tìm kiếm URL và phương thức yêu cầu
     request_line = request_lines[0].decode()
-    print(request_line)
     request_method, url, _ = request_line.split()
 
-    #if url==data:
-        #return  
+    # Xử lý URL
+    #http://oosc.online/
+
+    if url.startswith("http://"):
+        url = url[7:]
+    elif url.startswith("https://"):
+        url = url[8:]
+
+    # Tách thông tin hostname và port (nếu có)
+    #testphp.vulnweb.com/login.php
+    if '/' in url:
+        host_path = url.split('/', 1)
+        host = host_path[0]
+        path = '/' + host_path[1]
+    #oosc.online
+    else:
+        host = url
+        path = '/'
+    #kshdfskjh:12124
+    if ':' in host:
+        remote_host, remote_port = host.split(':', 1)
+        remote_port = int(remote_port)
+    else:
+        remote_host = host
+        remote_port = 80
     arrayName = ["GET", "POST", "HEAD"]
-    if request_method in arrayName and is_within_time_range(config):
-        # Xử lý URL
-        #http://oosc.online/
-
-        if url.startswith("http://"):
-            url = url[7:]
-        elif url.startswith("https://"):
-            url = url[8:]
-
-        # Tách thông tin hostname và port (nếu có)
-        #testphp.vulnweb.com/login.php
-        if '/' in url:
-            host_path = url.split('/', 1)
-            host = host_path[0]
-            path = '/' + host_path[1]
-        #oosc.online
-        else:
-            host = url
-            path = '/'
-        #kshdfskjh:12124
-        if ':' in host:
-            remote_host, remote_port = host.split(':', 1)
-            remote_port = int(remote_port)
-        else:
-            remote_host = host
-            remote_port = 80
-
+    if request_method in arrayName and is_within_time_range(config) and host in config['whitelisting']:
         # Kết nối tới máy chủ web từ xa
         remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         remote_socket.connect((remote_host, remote_port))
@@ -96,7 +96,6 @@ def handle_client(client_socket):
     else:
          Error403(client_socket)
          client_socket.close()
-
 
 def proxy_server():
     local_host = '127.0.0.1'  # Địa chỉ IP của máy cục bộ (localhost)
